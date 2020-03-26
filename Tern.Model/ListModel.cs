6684e93b6fd40a1aa291e69875d35108b8d36031
Rahul Chakrabarty <rahulchrty@gmail.com>
2020-03-26 21:59:05 +0530

    Added List Actions

---
+++ Tern.Model/ListModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Tern.Model
+{
+    public class ListModel
+    {
+        public int ListId { get; set; }
+        public string ListName { get; set; }
+        public List<TaskModel> Tasks { get; set; }
+    }
+}
